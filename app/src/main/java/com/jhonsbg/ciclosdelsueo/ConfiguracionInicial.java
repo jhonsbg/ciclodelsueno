f4795dc13092654445ba6f2a719ebb3e35084cac
Jhon Sebastian Bohorquez Guerrero <js.bohorquezg1@uniandes.edu.co>
2023-03-16 08:44:26 -0500

    Initial commit

---
+++ app/src/main/java/com/jhonsbg/ciclosdelsueo/ConfiguracionInicial.java
@@ -0,0 +1,14 @@
+package com.jhonsbg.ciclosdelsueo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfiguracionInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracion_inicial);
+    }
+}
